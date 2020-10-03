c1d108d0974d08c5d41476cb58b8ac03b1154ced
Kyotomeister <aloverrrrr@gmail.com>
2020-10-03 18:30:10 +0500

    init: making typical array problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!Array.isArray(array) || !array.length) {
+    return 0;
+  }
+  array.sort((a, b) => a - b) 
+  return array[0];
+  }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) {
+    return 0;
+  }
+  array.sort((a, b) => a - b) 
+  return array[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) {
+    return 0;
+  }
+  var sum = 0;
+  var count = array.length;
+  for (i=0; i<count; i++) {
+    sum += array[i];
+  }
+  return sum/count;
 }
